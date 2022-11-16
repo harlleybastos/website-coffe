e31f212ffb57778700c1ad3db91ea517f114d49b
Harlley Bastos <harlleybastos@hotmail.com>
2022-11-15 21:29:36 -0300

    styles: improve responsivity

--- src/components/Body/index.tsx
+++ src/components/Body/index.tsx
@@ -1,12 +1,59 @@
 import React, { useState } from "react";
 import Image from "../../assets/img/image/capsula.png";
 import ImageTwo from "../../assets/img/image/xicara-branca.png";
-import ImageThree from "../../assets/img/image/coffee-cup.png";
+import ImageTree from "../../assets/img/image/coffee-cup.png";
 import ImageArrow from "../../assets/img/image/arrow.png";
 
 const Body = () => {
   const [currentImage, setCurrentImage] = useState<string>(Image);
-  const [currentRight, setCurrentRight] = useState<number>(50);
+  const [currentRight, setCurrentRight] = useState<string>(
+    "left-[-2rem] sm:left-[14rem] md:left-[14rem] lg:left-[32rem] xl:left-[55rem]"
+  );
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+
+  const content = [
+    {
+      image: Image,
+      onClick: (index: number) => {
+        setCurrentIndex(index);
+        setCurrentImage(Image);
+        setCurrentRight(
+          "left-[-1rem] md:left-[15rem] lg:left-[25rem] xl:left-[55rem]"
+        );
+      },
+    },
+    {
+      image: ImageTwo,
+      onClick: (index: number) => {
+        setCurrentIndex(index);
+        setCurrentImage(ImageTwo);
+        setCurrentRight(
+          "left-[10rem]  md:left-[25rem] lg:left-[35rem] xl:left-[65rem]"
+        );
+      },
+    },
+    {
+      image: ImageTree,
+      onClick: (index: number) => {
+        setCurrentIndex(index);
+        setCurrentImage(ImageTree);
+        setCurrentRight(
+          "left-[21rem] md:left-[35rem] lg:left-[45rem] xl:left-[75rem]"
+        );
+      },
+    },
+  ];
+
+  interface ShapeContent {
+    [index: number]: string;
+  }
+
+  const contentText: ShapeContent = {
+    0: "Sunset Capuccino com Whey Protein",
+    1: "Sunset Xícara Personalizada",
+    2: "Sunset Cup Personalizado",
+  };
+
   return (
     <section
       className="home min-h-[100vh] pt-[12rem] bg-bg-img bg-no-repeat bg-center bg-cover"
@@ -14,61 +61,41 @@ const Body = () => {
     >
       <div className="row flex items-center flex-wrap gap-[1.5rem]">
         <div className="content flex-content">
-          <h3 className="text-[6.5rem] text-[#433] uppercase">
-            fresh coffee in the morning
+          <h3 className="text-[4rem] md:text-[4.5rem] xl:text-[6.5rem] text-[#433] uppercase">
+            {contentText[currentIndex]}
           </h3>
           <a href="#" className="btn">
-            buy one now
+            Comprar uma agora
           </a>
         </div>
 
-        <div className="image flex-content pt-[10rem] text-center">
+        <div className="image flex justify-center flex-content pt-[10rem] text-center">
           <img
             src={currentImage}
-            className="main-home-image h-[35rem] animate-[float_4s_linear_infinite]"
+            className="main-home-image h-[35rem] animate-[float_4s_linear_infinite] object-contain"
             alt=""
           />
         </div>
       </div>
-      <div className="image-slider flex items-center justify-center py-[3rem] px-[0]">
-        <img
-          className="h-[9rem] my-[0] mx-[0.5rem] mt-[5rem] hover:translate-y-[-2rem] relative"
-          src={Image}
-          alt=""
-          onClick={() => {
-            setCurrentImage(Image);
-            setCurrentRight(50);
-          }}
-        />
-        <img
-          className="h-[9rem] my-[0] mx-[0.5rem] mt-[5rem] hover:translate-y-[-2rem] relative"
-          src={ImageTwo}
-          alt=""
-          onClick={() => {
-            setCurrentImage(ImageTwo);
-            setCurrentRight(61);
-          }}
-        />
-        <img
-          className="h-[9rem] my-[0] mx-[0.5rem] mt-[5rem] hover:translate-y-[-2rem] relative"
-          src={ImageThree}
-          alt=""
-          onClick={() => {
-            setCurrentImage(ImageThree);
-            setCurrentRight(70);
-          }}
-        />
+      <div className="relative image-slider flex items-center justify-center py-[3rem] px-[0]">
         <div
-          className={`w-full absolute right-0 ${
-            currentRight === 50 ? "left-[50rem]" : `left-[${currentRight}rem]`
-          } bottom-20`}
+          className={`absolute bottom-[15rem] md:left-[30rem] md:bottom-[14rem] lg:bottom-[14rem] xl:bottom-[14rem] ${currentRight} lg:right-0`}
         >
           <img
-            className={`animate-[float_2s_linear_infinite] `}
+            className={`animate-[float_2s_linear_infinite] w-[10rem] lg:w-[12rem] xl:w-[18rem]`}
             src={ImageArrow}
             alt=""
           />
         </div>
+        {content.map((item, index) => (
+          <img
+            key={index}
+            className="h-[9rem] my-[0] mx-[0.5rem] mt-[5rem] hover:translate-y-[-2rem]"
+            src={item.image}
+            alt=""
+            onClick={() => item.onClick(index)}
+          />
+        ))}
       </div>
     </section>
   );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,18 +7,18 @@ import Logo from "../../assets/img/logo-separed.png";
 const Header = () => {
   const [selectedItem, setSelectedItem] = useState(0);
   return (
-    <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+    <nav className="bg-gray-900 border-gray-200 px-[9%] sm:px-4 py-[2rem] rounded">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
         <a href="https://flowbite.com/" className="flex items-center">
-          <img src={Logo} className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
-          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+          <img src={Logo} width={35} className="mr-5" />
+          <span className="self-center text-xl lg:text-[2.3rem] font-semibold whitespace-nowrap text-white">
             Sunset Coffee
           </span>
         </a>
         <button
           data-collapse-toggle="navbar-default"
           type="button"
-          className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          className="inline-flex items-center p-2 ml-3 text-sm rounded-lg md:hidden focus:outline-none focus:ring-2 text-gray-400 hover:bg-gray-700 focus:ring-gray-600"
           aria-controls="navbar-default"
           aria-expanded="false"
         >
@@ -38,11 +38,11 @@ const Header = () => {
           </svg>
         </button>
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul className="flex flex-col p-4 mt-4 rounded-lg border md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 bg-gray-800 md:bg-gray-900 border-gray-700">
             <li>
               <a
                 href="#"
-                className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-[#fe881a] md:p-0 dark:text-white"
+                className="block py-2 text-[1.7rem] pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-[#fe881a] md:p-0"
                 aria-current="page"
               >
                 Home
@@ -51,7 +51,7 @@ const Header = () => {
             <li>
               <a
                 href="#"
-                className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-[#ffc48d] dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className="block py-2 text-[1.7rem] pr-4 pl-3 rounded md:border-0 md:p-0 text-gray-400 md:hover:text-[#ffc48d] hover:bg-gray-700 hover:text-white md:hover:bg-transparent"
               >
                 About
               </a>
@@ -59,7 +59,7 @@ const Header = () => {
             <li>
               <a
                 href="#"
-                className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className="block py-2 text-[1.7rem] pr-4 pl-3 rounded md:border-0 md:p-0 text-gray-400 md:hover:text-white hover:bg-gray-700 hover:text-white"
               >
                 Menu
               </a>
@@ -67,7 +67,7 @@ const Header = () => {
             <li>
               <a
                 href="#"
-                className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className="block py-2 text-[1.7rem] pr-4 pl-3 rounded md:border-0 md:p-0 text-gray-400 md:hover:text-white hover:bg-gray-700 hover:text-white"
               >
                 Review
               </a>
@@ -75,7 +75,7 @@ const Header = () => {
             <li>
               <a
                 href="#"
-                className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className="block py-2 text-[1.7rem] pr-4 pl-3 rounded md:border-0 md:p-0 text-gray-400 md:hover:text-white hover:bg-gray-700 hover:text-white md:hover:bg-transparent"
               >
                 Book
               </a>
